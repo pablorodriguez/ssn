18cbd10ec307b26a4becdc1a92e1fccee5f31248
Pablo Rodriguez <pablorodriguez.ar@gmail.com>
2020-07-11 12:40:42 -0300

    rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
 
+class User < ApplicationRecord
   validates :first_name, presence: true
   has_one_attached :photo
 
   def thumbnail_photo
-    return self.photo.variant(resize: '250x300!')
+    photo.variant(resize: '250x300!')
   end
 
   def full_name
